4c7837bd844b638fbfa3d21ac77ffe57123ff49f
Benjamin Viramontes <benja120599@gmail.com>
2019-03-15 19:25:09 -0600

    Se añadió la clase Util
    En la clase util se declara un método estatico llamado sleep

--- src/GFG.java
+++ src/GFG.java
@@ -1,4 +1,12 @@
-
-public class GFG {
-
-}
+// Java program to illustrate Deadlock 
+// in multithreading. 
+class Util { 
+    // Util class to sleep a thread 
+    static void sleep(long millis) { 
+        try { 
+            Thread.sleep(millis); 
+        } catch (InterruptedException e) { 
+            e.printStackTrace(); 
+        } 
+    } 
+} 
