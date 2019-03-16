732285ce2815b3f71fc4972acfc34d724702db15
Benjamin Viramontes <benja120599@gmail.com>
2019-03-15 19:31:38 -0600

    Se añadió la clase Thread2
    En la clase Thread2 se añadió un constructor y el método run

--- src/GFG.java
+++ src/GFG.java
@@ -59,4 +59,26 @@ class Thread1 extends Thread {
         s1.test1(s2); 
     } 
     
-} 
+}
+
+class Thread2 extends Thread { 
+    
+	private Shared s1; 
+    private Shared s2; 
+  
+    // constructor to initialize fields 
+    public Thread2(Shared s1, Shared s2) { 
+        this.s1 = s1; 
+        this.s2 = s2; 
+    } 
+  
+    // run method to start a thread 
+    @Override
+    public void run() { 
+        // taking object lock of s2 
+        // enters into test2 method 
+        s2.test2(s1); 
+    } 
+
+}
+
